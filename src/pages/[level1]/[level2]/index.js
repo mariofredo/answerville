8a065acfffb22f8cf10e7387ffc1cb9658774a35
mariofredo <54829954+mariofredo@users.noreply.github.com>
2024-01-31 18:03:04 +0700

    first commit

--- src/pages/[level1]/[level2]/index.js
+++ src/pages/[level1]/[level2]/index.js
@@ -1,11 +1,11 @@
-'use client'
+'use client';
 import ArticleList from '@/components/ArticleList/ArticleList';
-import { useRouter } from 'next/router';  // Correct import statement
-import { useEffect, useState } from 'react';
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
 
 const ListPage = () => {
   const router = useRouter();
-  const { level1, level2, article } = router.query;
+  const {level1, level2, article} = router.query;
   const [data, setData] = useState([]);
   const [fetchedData, setFetchedData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -29,9 +29,9 @@ const ListPage = () => {
     const fetchData = async () => {
       try {
         if (level2) {
-          const found = fetchedData.find(key => key.name === level1)
+          const found = fetchedData.find((key) => key.name === level1);
           console.log(found);
-          const id = found.level_2.find(key => key.name === level2)
+          const id = found.level_2.find((key) => key.name === level2);
           const apiUrl = `${process.env.NEXT_PUBLIC_API_HOST}/article?category=${id.id}`;
           const response = await fetch(apiUrl);
           const result = await response.json();
@@ -44,21 +44,28 @@ const ListPage = () => {
         setLoading(false);
       }
     };
-    if(fetchedData.length>0){
+    if (fetchedData.length > 0) {
       fetchData();
     }
-  }, [fetchedData]);  // Run the effect whenever article changes
+  }, [fetchedData]); // Run the effect whenever article changes
 
-//   if (!article) {
-//     // Render loading state or fallback content when article is not available
-//     return <p>Loading...</p>;
-//   }
+  //   if (!article) {
+  //     // Render loading state or fallback content when article is not available
+  //     return <p>Loading...</p>;
+  //   }
 
   return (
     <section className='article_section'>
       <div className='article_list'>
         {data.map((article) => (
-          <ArticleList key={article.slug} slug={article.slug} thumbnail={article.thumbnail} title={article.title} created_at={article.created_at} summary={article.summary}  />
+          <ArticleList
+            key={article.slug}
+            slug={article.slug}
+            thumbnail={article.thumbnail}
+            title={article.title}
+            created_at={article.created_at}
+            summary={article.summary}
+          />
         ))}
       </div>
       {/* {loading ? (
