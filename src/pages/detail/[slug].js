77b5753c6bb90cb32ffc0c6ab2e758b04eace4bf
mariofredo <54829954+mariofredo@users.noreply.github.com>
2024-01-31 18:37:27 +0700

    modify url img detail page

--- src/pages/detail/[slug].js
+++ src/pages/detail/[slug].js
@@ -1,12 +1,12 @@
 // pages/article/[slug].js
-'use client'
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import "../../styles/main.css"
+'use client';
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
+import '../../styles/main.css';
 
 const ArticleDetail = () => {
   const router = useRouter();
-  const { slug } = router.query;
+  const {slug} = router.query;
   const [article, setArticle] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -21,7 +21,7 @@ const ArticleDetail = () => {
             headers: {
               'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ slug }),
+            body: JSON.stringify({slug}),
           });
 
           const result = await response.json();
@@ -36,7 +36,7 @@ const ArticleDetail = () => {
     };
 
     fetchData();
-  }, [slug]);  // Run the effect whenever slug changes
+  }, [slug]); // Run the effect whenever slug changes
 
   if (loading) {
     // Render loading state or fallback content when article is not available
@@ -53,39 +53,41 @@ const ArticleDetail = () => {
   //   (section) => section.type === 'image'
   // );
 
-
   // // Construct the full image URL
   // const fullImageUrl = `${process.env.NEXT_PUBLIC_API_HOST.replace('api', '')}${imageSection.image}`;
 
   const imageSection = Object.values(article.content).find(
     (section) => section.type === 'image'
   );
-  
+
   // Construct the full image URL if imageSection exists and has a valid image property
-  const fullImageUrl = imageSection && imageSection.image
-    ? `${process.env.NEXT_PUBLIC_API_HOST.replace('api', '')}${imageSection.image}`
-    : null;
-  
+  const fullImageUrl =
+    imageSection && imageSection.image
+      ? `${process.env.NEXT_PUBLIC_API_HOST.replace('api/', '')}/${
+          imageSection.image
+        }`
+      : null;
 
   return (
     <section className='article_detail_section'>
       <div className='container'>
-      <h1>{article.title}</h1>
+        <h1>{article.title}</h1>
 
-      {/* Render image section */}
-      {imageSection && (
-        <img src={fullImageUrl} alt={`Image`} />
-      )}
+        {/* Render image section */}
+        {imageSection && <img src={fullImageUrl} alt={`Image`} />}
 
-      {/* Render text sections */}
-      {article.content &&
-        Object.values(article.content)
-          .filter((section) => section.type === 'text')
-          .map((section, index) => (
-            <div key={index} dangerouslySetInnerHTML={{ __html: section.text }} />
-          ))}
+        {/* Render text sections */}
+        {article.content &&
+          Object.values(article.content)
+            .filter((section) => section.type === 'text')
+            .map((section, index) => (
+              <div
+                key={index}
+                dangerouslySetInnerHTML={{__html: section.text}}
+              />
+            ))}
 
-      {/* Add other details you want to display */}
+        {/* Add other details you want to display */}
       </div>
     </section>
   );
