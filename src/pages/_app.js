6a6a86b07b48be8eff70e0119d371ef41d05f2a8
Kurniadi17 <kurniadikurniadi1@gmail.com>
2024-04-02 14:45:56 +0700

    add google tag manager inside of _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,19 @@
 // _app.js atau _app.jsx
 
 import React from 'react';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import Head from '@/components/Head/Head.js'
+import {GoogleTagManager} from '@next/third-parties/google';
+import Head from '@/components/Head/Head.js';
 import Header from '@/components/Header';
-import "../../public/fonts/stylesheet.css"
-import "../styles/main.css";
+import '../../public/fonts/stylesheet.css';
+import '../styles/main.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   return (
     <>
       <Head />
       <Header />
       <Component {...pageProps} />
+      <GoogleTagManager gtmId='G-JNZC27WEH5' />
     </>
   );
 }
